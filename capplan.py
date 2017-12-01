60ffd8bc32d2d33a83fa8a8a2c558237ab592f8f
Ronald <r.grefhorst@gmail.com>
2017-12-01 14:16:16 +0100

    Moved example code to example.py

--- capplan.py
+++ capplan.py
@@ -175,6 +175,7 @@ class Project(Serial):
         super().__init__(initlist=initlist, deadline=deadline, title=title)
         self.data.append(Milestone(slack, "Milestone"))
 
+
 def serialize(activity):
     def serialize_task(task):
         ser = {'title': task.title,
@@ -236,39 +237,3 @@ def deserialize(data):
         return deserialize_project(data, project_types[at])
     else:
         raise KeyError('cannot find deserializer')
-
-
-def example_project():
-    coll1 = Serial([Task(1, "T1", resources=["piet", "klaas"]), Task(1, "T2"), Task(2, "T3")])
-    coll2 = Parallel([Task(1, "T4"), Task(2, "T5")])
-    coll3 = Serial([coll2, Task(1, "T6")])
-    coll4 = Serial([Task(1, "T7"), Task(1, "T8"), Task(3, "T9")])
-    coll5 = Parallel([Task(2, "T10"), Task(3, "T11")])
-    coll6 = Parallel([coll1, coll3, coll4, coll5])
-    coll7 = Serial([Task(1, "T12"), Task(3, "T13")])
-    coll8 = Parallel([coll7, Task(2, "T14")])
-    collection = Serial([coll6, coll8])
-    p = Project(collection, 11, "Ship development", slack=2)
-    return p
-
-
-def main():
-    from plotters.plotter import MplPlotter
-    p = example_project()
-    p.task("T2").progress = 0.5
-
-    s = serialize(p)
-    print(s)
-    p2 = deserialize(s)
-    # print(Task.deserialize(s))
-    print(str(p2))
-
-    # p.plan()
-    p2.plan()
-    plotter = MplPlotter()
-    plotter.plot_project(p2)
-    plotter.show()
-
-
-if __name__ == '__main__':
-    main()
